e82e470b7250207357ed6bd0266358f0cbb55bb7
Kou <ioiowax2020.jp@gmail.com>
2021-05-24 00:20:16 +0900

    feat: Req time stamp

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>時間戳記 time stamp</h1>
+
+<span>能夠紀錄私服器 req,res 時間</span>
+
+<h2>功能列表</h2>
+
+
+<li>紀錄伺服器req日期時間</li>
+<li>紀錄伺服器res日期時間</li>
+<li>紀錄伺服器反應的時間</li>
+
+<span> 終端機輸入</span>
+```bash
+npm run dev //可開啟此專案
+```
+
+<h2>環境建置與需求</h2>
+<li>Visual Studio Code 2 -開發環境</li>
+<li>Express ^4.17.1 - 應用程式架構</li>
---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.use((req, res, next) => {
+
+  const reqDate = new Date()
+
+
+  const tolocalstring = reqDate.toISOString()
+  const totimeString = reqDate.toTimeString()
+  const Methods = req.method
+  const urlbase = req.originalUrl
+
+  let splitTime = totimeString.split(' ')
+  let splitDate = tolocalstring.split('T')
+
+
+  console.log(`${splitDate[0]}  ${splitTime[0]} |  ${Methods} from ${urlbase}`)
+
+
+
+})
+
+app.get('/', (req, res, next) => {
+
+  res.send('列出全部 Todo')
+
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+
+})
+
+app.get('/:id', (req, res) => {
+
+  res.send('顯示一筆 Todo')
+
+})
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
+
+
+function currentTime(res, req, next) {
+  const nowDate = new Date()
+
+  const tolocalstring = nowDate.toISOString()
+  const totimeString = nowDate.toTimeString()
+  const Methods = req.method
+  const urlbase = req.originalUrl
+
+  let splitTime = totimeString.split(' ')
+  let splitDate = tolocalstring.split('T')
+
+  const formalTime = (`${splitDate[0]}  ${splitTime[0]} |  ${Methods} from ${urlbase}$`)
+  console.log(formalTime)
+
+}
